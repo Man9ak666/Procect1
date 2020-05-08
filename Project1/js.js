9c90000fe9d0c792ca1d5cf92b999379633196e3
Mark <mkpodporacz@gmail.com>
2020-05-08 13:13:53 +0200

    the first commit

---
+++ Project1/js.js
@@ -0,0 +1,122 @@
+'use strict';
+// // let persone =
+// //     name: "Mark",
+// //     age: 13,
+// //     secondname: "Smorodin",
+
+// // };
+
+// // console.log(persone.nae)
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// // // console.log(arr[0]);
+
+
+// // // 'string',"string",`string`;
+// // // // true/ false
+
+// // // let a;
+// // // console.log(a)
+
+// // // const obj {
+// // //     name: 'Alex'
+// // // }
+// // //             0   1   2   3
+// // // const arr = [1, 2, 4, 5, 6]
+
+// // 1)
+// // console.log(typeof(String(4)));
+// // 2)
+// // // console.log(typeof("q " + "5"));
+// // console.log('https://vk.com/catalog/' + 5)
+
+// // 1)
+// console.log(typeof(Number('4')));
+// // 2)
+// console.log(typeof(5 + +'5'));
+// // 3)
+// console.log(typeof(parseInt('14px', 10)));
+
+// // let ans = +prompt("Hello?", '123');
+// // nothing
+// // 0,'', null,undefined,NaN
+
+// let switcher = null;
+
+// if(switcher) {
+//     console.log("working...")
+// }
+
+// switcher = 1;
+
+// if(switcher) {
+//     console.log("working...")
+// }
+
+// // 2)
+
+// console.log(typeof(Boolean('4')));
+// // 3)
+// console.log(typeof(!!'5'));
+
+// var i = 5
+
+// var a = 5
+// function go() {
+// var a = 6
+// }
+// go()
+
+// console.log(go)
+
+// if(var a=1) alert("BOOM");
+// console.log(var a=1)
+
+// var apples = 5;
+
+// if (true) {
+//   var apples = 10;
+
+//   alert(apples); // 10 (внутри блока)
+// }
+
+// alert(apples); // 10 (снаружи блока то же самое)
+
+// console.log()
+// var
+
+// var x = 1;
+// if (x === 1) {
+//     var x = 2;
+// }
+// console.log(x)
+
+// alert("hello World")
+// let answer = confirm("Are you here?")
+
+// console.log(answer);
+
+// let answer = +prompt("je vam 18", "yes");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose);
+
